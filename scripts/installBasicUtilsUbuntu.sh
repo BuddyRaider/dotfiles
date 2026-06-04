7beb97ced00f4e475ea9c32fc3461a7eda9ead41
Nicholas Gillroy <104165073+BuddyRaider@users.noreply.github.com>
2026-06-04 17:34:12 -0400

    Add installBasicUtilsUbuntu.sh

---
+++ scripts/installBasicUtilsUbuntu.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo apt install vlc mpv audacity kdenlive obs-studio ffmpeg steam qutebrowser konqueror libreoffice okular gimp krita kdeconnect bleachbit flatpak filelight conky-all kate vim openssh-client nmap wireguard python3 nodejs npm git git-lfs lazygit docker.io virtualbox btop htop fastfetch bat fzf wget curl tmux cmake rsync ranger cpu-x handbrake kodi haruna shortwave antimicrox mangohud goverlay
